83efa4705b0666f71162a7999a6a7d1faa43d6c4
khoualed2u <rayan.khoualed3@etu.univ-lorraine.fr>
2023-12-06 19:27:50 +0100

    Deplacement entre les pièces presques fini, manque de regarder la direction lors du changement de pièce

--- app/src/main/java/com/example/visualisationmodele/PieceActivity.java
+++ app/src/main/java/com/example/visualisationmodele/PieceActivity.java
@@ -2,17 +2,30 @@ package com.example.visualisationmodele;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.PixelFormat;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
+import java.util.ArrayList;
+
+import habitation.Ouverture;
+import habitation.Piece;
 import outils.ModeleSingleton;
 
 public class PieceActivity extends AppCompatActivity {
@@ -22,8 +35,11 @@ public class PieceActivity extends AppCompatActivity {
     private Bitmap bitmapSud;
     private Bitmap bitmapEst;
     private Bitmap bitmapOuest;
+    private Canvas canvas;
     private String direction;
     private SurfaceHolder surfaceHolder;
+    private ArrayList<Rect> rects;
+    private Paint paint;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +49,14 @@ public class PieceActivity extends AppCompatActivity {
     public void init(){
         surfaceView = findViewById(R.id.surfaceView);
         imageView = findViewById(R.id.imageView_Mur);
+        rects = new ArrayList<>();
         surfaceHolder = surfaceView.getHolder();
         surfaceView.setZOrderOnTop(true);
+        canvas = new Canvas();
+        paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.GREEN);
+        paint.setStrokeWidth(6);
         surfaceHolder.setFormat(PixelFormat.TRANSPARENT);
         bitmapNord= BitmapFactory.decodeByteArray(ModeleSingleton.getInstance().getPieceEnCours().getMurNord().getBytes(), 0, ModeleSingleton.getInstance().getPieceEnCours().getMurNord().getBytes().length);
         bitmapOuest= BitmapFactory.decodeByteArray(ModeleSingleton.getInstance().getPieceEnCours().getMurOuest().getBytes(), 0, ModeleSingleton.getInstance().getPieceEnCours().getMurOuest().getBytes().length);
@@ -42,6 +64,135 @@ public class PieceActivity extends AppCompatActivity {
         bitmapEst= BitmapFactory.decodeByteArray(ModeleSingleton.getInstance().getPieceEnCours().getMurEst().getBytes(), 0, ModeleSingleton.getInstance().getPieceEnCours().getMurEst().getBytes().length);
         direction = "nord";
         imageView.setImageBitmap(bitmapNord);
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent motionEvent) {
+                int x = (int) motionEvent.getX(0);
+                int y = (int) motionEvent.getY(0);
+                Ouverture ouvertureCliquee = getOuvertureClic(x, y);
+                Log.i("Piece","X: "+x +" Y : "+y);
+                Log.i("Piece","Click");
+                if (ouvertureCliquee != null) {
+                    Log.i("Piece","ouverture pas null");
+                    Piece p = trouverPieceParNom(ouvertureCliquee.getPieceArrivee());
+                    verifChangementPiece(p);
+                }
+                return false;
+            }
+        });
+    }
+    public void verifChangementPiece(Piece p) {
+        if (p.getMurEst().getNomBitmap() != null && p.getMurSud().getNomBitmap() != null && p.getMurNord().getNomBitmap() != null && p.getMurOuest().getNomBitmap() != null) {
+            ModeleSingleton.getInstance().setPieceEnCours(p);
+            initNewPiece();
+        }else {
+            new AlertDialog.Builder(this)
+                    .setTitle("Piece invalide")
+                    .setMessage("La pièce doit avoir 4 pièces pour la visiter.")
+                    .setNeutralButton("OK",null)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+    }
+    public Piece trouverPieceParNom(String nom) {
+        for (Piece piece : ModeleSingleton.getInstance().getModeleInstance().getPieceArrayList()) {
+            if (piece.getNom().equals(nom)) {
+                return piece;
+            }
+        }
+        return null; // Pièce non trouvée
+    }
+    private Ouverture getOuvertureClic(float x, float y) {
+        switch (direction){
+            case "nord":
+                for (Ouverture ouverture : ModeleSingleton.getInstance().getPieceEnCours().getMurNord().getOuvertures()) {
+                    if (ouverture.getRect().contains((int) x, (int) y)) {
+                        Log.i("Piece",ouverture.toString());
+                        return ouverture;
+                    }
+                }
+                break;
+            case "est":
+                for (Ouverture ouverture : ModeleSingleton.getInstance().getPieceEnCours().getMurEst().getOuvertures()) {
+                    if (ouverture.getRect().contains((int) x, (int) y)) {
+                        Log.i("Piece",ouverture.toString());
+                        return ouverture;
+                    }
+                }
+                break;
+            case"ouest":
+                for (Ouverture ouverture : ModeleSingleton.getInstance().getPieceEnCours().getMurOuest().getOuvertures()) {
+                    if (ouverture.getRect().contains((int) x, (int) y)) {
+                        Log.i("Piece",ouverture.toString());
+                        return ouverture;
+                    }
+                }
+                break;
+            case "sud":
+                for (Ouverture ouverture : ModeleSingleton.getInstance().getPieceEnCours().getMurSud().getOuvertures()) {
+                    if (ouverture.getRect().contains((int) x, (int) y)) {
+                        Log.i("Piece",ouverture.toString());
+                        return ouverture;
+                    }
+                }
+                break;
+        }
+
+        return null;
+    }
+    public void initNewPiece(){
+        bitmapNord= BitmapFactory.decodeByteArray(ModeleSingleton.getInstance().getPieceEnCours().getMurNord().getBytes(), 0, ModeleSingleton.getInstance().getPieceEnCours().getMurNord().getBytes().length);
+        bitmapOuest= BitmapFactory.decodeByteArray(ModeleSingleton.getInstance().getPieceEnCours().getMurOuest().getBytes(), 0, ModeleSingleton.getInstance().getPieceEnCours().getMurOuest().getBytes().length);
+        bitmapSud= BitmapFactory.decodeByteArray(ModeleSingleton.getInstance().getPieceEnCours().getMurSud().getBytes(), 0, ModeleSingleton.getInstance().getPieceEnCours().getMurSud().getBytes().length);
+        bitmapEst= BitmapFactory.decodeByteArray(ModeleSingleton.getInstance().getPieceEnCours().getMurEst().getBytes(), 0, ModeleSingleton.getInstance().getPieceEnCours().getMurEst().getBytes().length);
+        direction = "nord";
+        imageView.setImageBitmap(bitmapNord);
+    }
+    public void affichageRect(){
+        rects.clear();
+        switch (direction){
+            case "nord":
+                for(Ouverture ouverture : ModeleSingleton.getInstance().getPieceEnCours().getMurNord().getOuvertures()){
+                    rects.add(ouverture.getRect());
+                }
+                break;
+            case "sud":
+                for(Ouverture ouverture : ModeleSingleton.getInstance().getPieceEnCours().getMurSud().getOuvertures()){
+                    rects.add(ouverture.getRect());
+                }
+                break;
+            case "ouest":
+                for(Ouverture ouverture : ModeleSingleton.getInstance().getPieceEnCours().getMurOuest().getOuvertures()){
+                    rects.add(ouverture.getRect());
+                }
+                break;
+            case "est":
+                for(Ouverture ouverture : ModeleSingleton.getInstance().getPieceEnCours().getMurEst().getOuvertures()){
+                    rects.add(ouverture.getRect());
+                }
+                break;
+        }
+        dessinRectangle();
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            affichageRect();
+        }
+    }
+    public void dessinRectangle() {
+        canvas = surfaceView.getHolder().lockCanvas();
+        if (canvas != null) {
+            try {
+                canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR); // Efface le contenu précédent
+                for (Rect r : rects)
+                    canvas.drawRect(r, paint);
+            } finally {
+                surfaceView.getHolder().unlockCanvasAndPost(canvas);
+            }
+        }
     }
 
     public void nextClick(View view) {
@@ -49,18 +200,22 @@ public class PieceActivity extends AppCompatActivity {
             case "nord":
                 direction = "est";
                 imageView.setImageBitmap(bitmapEst);
+                affichageRect();
                 break;
             case "sud":
                 direction = "ouest";
                 imageView.setImageBitmap(bitmapOuest);
+                affichageRect();
                 break;
             case "ouest":
                 direction = "nord";
                 imageView.setImageBitmap(bitmapNord);
+                affichageRect();
                 break;
             case "est":
                 direction = "sud";
                 imageView.setImageBitmap(bitmapSud);
+                affichageRect();
                 break;
         }
     }
@@ -70,18 +225,22 @@ public class PieceActivity extends AppCompatActivity {
             case "nord":
                 direction = "ouest";
                 imageView.setImageBitmap(bitmapOuest);
+                affichageRect();
                 break;
             case "sud":
                 direction = "est";
                 imageView.setImageBitmap(bitmapEst);
+                affichageRect();
                 break;
             case "ouest":
                 direction = "sud";
                 imageView.setImageBitmap(bitmapSud);
+                affichageRect();
                 break;
             case "est":
                 direction = "nord";
                 imageView.setImageBitmap(bitmapNord);
+                affichageRect();
                 break;
         }
     }
